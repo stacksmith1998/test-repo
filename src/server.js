552cd66f76f8ae96cfd001548a6c0f257cab1d6a
yscoder310 <gangbuster74@gmail.com>
2025-11-01 16:10:51 +0530

    add chnages

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.send({ status: "ok" });
+});
+
+app.listen(3000, () => {
+  console.log("SERVER IS RUNNING");
+});
